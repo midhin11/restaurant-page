98d0e2aa0478c402ea8b08425e0f816b23c94279
midhin11 <midhincr7@gmail.com>
2026-03-18 11:48:18 +0530

    Create home page by hardcoding HTML and CSS

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ export default {
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
